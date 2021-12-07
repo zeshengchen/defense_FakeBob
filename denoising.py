0a11143ab9c8f095b462ae738749bcb5f616a7c4
Zesheng Chen <chenz@pfw.edu>
2021-12-07 15:52:49 -0500

    Format denoising.py file

--- denoising.py
+++ denoising.py
@@ -1,16 +1,17 @@
 # Denoising method
-import numpy as np 
-from noisereducev1 import reduce_noise 
+import numpy as np
+from noisereducev1 import reduce_noise
+
 
 def denoise(audios, noise_std):
-	mean = 0
-	noise = np.random.normal(mean, noise_std, size = len(audios))
-	
-	out_audios = reduce_noise(audios,
-							  noise,
-							  n_fft = 512,
-							  win_length = 400,
-							  hop_length = 160
-							 )
+    mean = 0
+    noise = np.random.normal(mean, noise_std, size=len(audios))
 
-	return out_audios
+    out_audios = reduce_noise(audios,
+                              noise,
+                              n_fft=512,
+                              win_length=400,
+                              hop_length=160
+                              )
+
+    return out_audios
